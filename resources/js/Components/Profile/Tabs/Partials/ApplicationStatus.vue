617f409c9938598788ec4ae75eae7563de6e8106
novice-is-me <reginajalandoni13@gmail.com>
2025-04-23 23:33:19 +0800

    Updating status color

--- resources/js/Components/Profile/Tabs/Partials/ApplicationStatus.vue
+++ resources/js/Components/Profile/Tabs/Partials/ApplicationStatus.vue
@@ -7,18 +7,18 @@ const props = defineProps({
     <div>
         <div
             v-if="status === 'Interview'"
-            class="bg-blue-200 text-blue-700 px-3 rounded-full"
+            class="bg-blue-200 text-blue-600 px-3 rounded-full"
         >
             <p class="text-[11px] font-semibold">Interview</p>
         </div>
         <div v-if="status === 'Reviewed'">
-            <p class="text-[11px] text-yellow-700 font-semibold">Reviewed</p>
+            <p class="text-[11px] text-green-600 font-semibold">Reviewed</p>
         </div>
         <div v-if="status === 'Rejected'">
-            <p class="text-[11px] text-red-700 font-semibold">Rejected</p>
+            <p class="text-[11px] text-red-600 font-semibold">Rejected</p>
         </div>
         <div v-if="status === 'Pending'">
-            <p class="text-[11px] text-green-700 font-semibold">Pending</p>
+            <p class="text-[11px] text-yellow-600 font-semibold">Pending</p>
         </div>
     </div>
 </template>
